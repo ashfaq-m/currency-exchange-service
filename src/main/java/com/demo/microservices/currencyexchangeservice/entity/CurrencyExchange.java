343aa0effaa4e72fad61a294166a6babdf0a8a37
ashfaq-m <ashapak.makandar7@gmail.com>
2024-08-07 12:35:32 +0530

    Added MySQL database connection

--- src/main/java/com/demo/microservices/currencyexchangeservice/entity/CurrencyExchange.java
+++ src/main/java/com/demo/microservices/currencyexchangeservice/entity/CurrencyExchange.java
@@ -1,5 +1,9 @@
 package com.demo.microservices.currencyexchangeservice.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +13,17 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "currencyExchange")
 public class CurrencyExchange {
 
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
